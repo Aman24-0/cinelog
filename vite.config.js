e06eb5e381afe866217948696ea2bccb024fad42
Aman Dahayat <89609354+Aman24-0@users.noreply.github.com>
2026-04-27 00:35:53 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,26 +7,9 @@ export default defineConfig({
     solidPlugin(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
-      manifest: false, // Hum public folder wala manifest use karenge
+      manifest: false, 
       workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/image\.tmdb\.org\/.*/i,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'tmdb-images',
-              expiration: {
-                maxEntries: 100,
-                maxAgeSeconds: 60 * 60 * 24 * 30, // 30 Days
-              },
-              cacheableResponse: {
-                statuses: [0, 200],
-              },
-            },
-          },
-        ],
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
       }
     })
   ],
