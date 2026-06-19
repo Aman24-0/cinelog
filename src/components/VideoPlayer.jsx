465851f2c9fde60a6a872a8ccacfa8f7303b1d2a
Aman Dahayat <89609354+Aman24-0@users.noreply.github.com>
2026-06-19 13:58:32 +0530

    Update VideoPlayer.jsx

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -2,7 +2,7 @@ import { onMount, onCleanup } from 'solid-js';
 import videojs from 'video.js';
 import 'video.js/dist/video-js.css';
 
-export function VideoPlayer(props) {
+export default function VideoPlayer(props) {
   let videoRef;
   let player;
 
